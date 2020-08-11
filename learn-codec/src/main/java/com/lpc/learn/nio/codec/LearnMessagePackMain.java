027d93c7a392b3146a19b3a780f4c749a00b0211
lipengcheng3 <lipengcheng3@jd.com>
2020-08-11 10:15:14 +0800

    init: 初始化学习编解码器的demo

---
+++ learn-codec/src/main/java/com/lpc/learn/nio/codec/LearnMessagePackMain.java
@@ -0,0 +1,26 @@
+package com.lpc.learn.nio.codec;
+
+import org.msgpack.MessagePack;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: 李鹏程
+ * @email: lipengcheng3@jd.com
+ * @date: 2020/8/11
+ * @Time: 10:07
+ * @Description:
+ */
+public class LearnMessagePackMain {
+    public static void main(String[] args) throws IOException {
+        List<String> src = new ArrayList<>();
+        src.add("lipengcheng");
+        src.add("李鹏程");
+        src.add("1234567890");
+        MessagePack messagePack = new MessagePack();
+        byte[] raw = messagePack.write(src);
+        System.out.println(raw);
+    }
+}
