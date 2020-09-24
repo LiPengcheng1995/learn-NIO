8d64fcad58e0aa3f1e9fcc3ef04976d699e5a3a1
lipengcheng3 <lipengcheng3@jd.com>
2020-09-24 18:39:08 +0800

    finish: 完成保存

--- learn-netty-webSocket-server/src/main/java/com/lpc/learn/nio/netty/webSocket/server/handler/WebSocketHandler.java
+++ learn-netty-webSocket-server/src/main/java/com/lpc/learn/nio/netty/webSocket/server/handler/WebSocketHandler.java
@@ -2,6 +2,9 @@ package com.lpc.learn.nio.netty.webSocket.server.handler;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.websocketx.*;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author: 李鹏程
@@ -10,9 +13,76 @@ import io.netty.channel.SimpleChannelInboundHandler;
  * @Time: 15:26
  * @Description:
  */
+@Slf4j
 public class WebSocketHandler extends SimpleChannelInboundHandler<Object> {
+    private WebSocketServerHandshaker handshaker;
+
+
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
-
+        // 传统的HTTP接入
+        if (o instanceof FullHttpRequest) {
+            handleHttpRequest(channelHandlerContext, (FullHttpRequest) o);
+        }
+        // WebSocket接入
+        else if (o instanceof WebSocketFrame) {
+            handleWebSocketFrame(channelHandlerContext, (WebSocketFrame) o);
+        }
     }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+
+    private void handleHttpRequest(ChannelHandlerContext ctx,
+                                   FullHttpRequest req) throws Exception {
+
+        // 构造握手响应返回，本机测试
+        WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory(
+                "ws://localhost:8080/websocket", null, false);
+        handshaker = wsFactory.newHandshaker(req);
+        if (handshaker == null) {
+            WebSocketServerHandshakerFactory
+                    .sendUnsupportedWebSocketVersionResponse(ctx.channel());
+        } else {
+            // webSocket 和对应的上下文建立关系
+            handshaker.handshake(ctx.channel(), req);
+        }
+    }
+
+    private void handleWebSocketFrame(ChannelHandlerContext ctx,
+                                      WebSocketFrame frame) {
+
+        // 判断是否是关闭链路的指令
+        if (frame instanceof CloseWebSocketFrame) {
+            handshaker.close(ctx.channel(),
+                    (CloseWebSocketFrame) frame.retain());
+            return;
+        }
+        // 判断是否是Ping消息
+        if (frame instanceof PingWebSocketFrame) {
+            ctx.channel().write(
+                    new PongWebSocketFrame(frame.content().retain()));
+            return;
+        }
+        // 本例程仅支持文本消息，不支持二进制消息
+        if (!(frame instanceof TextWebSocketFrame)) {
+            throw new UnsupportedOperationException(String.format(
+                    "%s frame types not supported", frame.getClass().getName()));
+        }
+
+        // 返回应答消息
+        String request = ((TextWebSocketFrame) frame).text();
+        ctx.channel().write(
+                new TextWebSocketFrame(request
+                        + " , 欢迎使用Netty WebSocket服务，现在时刻："
+                        + new java.util.Date().toString()));
+    }
+
 }
